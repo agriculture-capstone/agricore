fb3aa513fcf9b661d4fe38ca081cc78de1c5aa7a
James Inglis <jamesekinglis@gmail.com>
2017-11-09 09:37:57 -0700

    made changes to dockerize.sh requested by reviewers: it no longer requires a second run script and uses yarn rather than npm

--- dockerize.sh
+++ dockerize.sh
@@ -3,14 +3,15 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 IMAGE_NAME="boresha/agriculture-core"
 DOCKER_HOME="/agiculture-core/"
-ARG_NUM=2
 
 if [ "$1" == "init" ]; then
 	docker build -t $IMAGE_NAME $DIR
 elif [ "$1" == "build" ]; then
-	docker run --rm -v $(pwd):/agriculture-core/ $IMAGE_NAME npm build
+    docker run --rm --volume "$DIR:$DOCKER_HOME" -ti $IMAGE_NAME yarn install --frozen-lockfile --no-bin-links
+    docker run --rm --volume "$DIR:$DOCKER_HOME" $IMAGE_NAME npm run build
 elif [ "$1" == "run" ]; then
-    docker run --rm --volume "$(pwd):/agriculture-core/" -ti agriculture-core ./start.sh
+    docker run --rm --volume "$DIR:$DOCKER_HOME" -ti $IMAGE_NAME yarn install --frozen-lockfile --no-bin-links
+    docker run --rm --volume "$DIR:$DOCKER_HOME" -ti -p 9090:9090 $IMAGE_NAME npm start
 else
 	echo "usage: docker_build [init | build | run]"
 fi
