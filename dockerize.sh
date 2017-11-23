ed21c9b424f7b1c6d80af73f242cf8b2e668df68
Enoch Tsang <etsang1@hotmail.com>
2017-11-23 01:59:07 -0700

    ag-59 fix typo and tabs to spaces

--- dockerize.sh
+++ dockerize.sh
@@ -2,23 +2,23 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 IMAGE_NAME="boresha/agriculture-core"
-DOCKER_HOME="/agiculture-core/"
+DOCKER_HOME="/agriculture-core/"
 
 if [ "$1" == "init" ]; then
-	docker build -t $IMAGE_NAME $DIR
+    docker build -t $IMAGE_NAME $DIR
 elif [ "$1" == "install" ]; then
     docker run --rm \
-    	--volume "$DIR:$DOCKER_HOME" \
-    	"$IMAGE_NAME" yarn install --frozen-lockfile
+        --volume "$DIR":"$DOCKER_HOME" \
+        "$IMAGE_NAME" yarn install --frozen-lockfile
 elif [ "$1" == "start" ]; then
     docker run --rm \
-    	--volume "$DIR:$DOCKER_HOME" \
-    	-p 9090:9090 \
-    	$IMAGE_NAME npm start
+        --volume "$DIR":"$DOCKER_HOME" \
+        -p 9090:9090 \
+        $IMAGE_NAME npm start
 elif [ "$1" == "run" ]; then
-	docker run --rm \
-		--volume "$DIR":"$DOCKER_HOME" \
-		"$IMAGE_NAME" npm run "${@:2}"
+    docker run --rm \
+        --volume "$DIR":"$DOCKER_HOME" \
+        "$IMAGE_NAME" npm run "${@:2}"
 else
-	echo "usage: $0 [init | install | start | run [args] ]"
+    echo "usage: $0 [ init | install | start | run [args] ]"
 fi
