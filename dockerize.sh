8bc11ffe3b22627037c1683d6623df6c74a34176
Enoch Tsang <etsang1@hotmail.com>
2017-11-23 01:54:13 -0700

    ag-59 update script and remove compose file

--- dockerize.sh
+++ dockerize.sh
@@ -6,12 +6,19 @@ DOCKER_HOME="/agiculture-core/"
 
 if [ "$1" == "init" ]; then
 	docker build -t $IMAGE_NAME $DIR
-elif [ "$1" == "build" ]; then
-    docker run --rm --volume "$DIR:$DOCKER_HOME" -ti $IMAGE_NAME yarn install --frozen-lockfile --no-bin-links
-    docker run --rm --volume "$DIR:$DOCKER_HOME" $IMAGE_NAME npm run build
+elif [ "$1" == "install" ]; then
+    docker run --rm \
+    	--volume "$DIR:$DOCKER_HOME" \
+    	"$IMAGE_NAME" yarn install --frozen-lockfile
+elif [ "$1" == "start" ]; then
+    docker run --rm \
+    	--volume "$DIR:$DOCKER_HOME" \
+    	-p 9090:9090 \
+    	$IMAGE_NAME npm start
 elif [ "$1" == "run" ]; then
-    docker run --rm --volume "$DIR:$DOCKER_HOME" -ti $IMAGE_NAME yarn install --frozen-lockfile --no-bin-links
-    docker run --rm --volume "$DIR:$DOCKER_HOME" -ti -p 9090:9090 $IMAGE_NAME npm start
+	docker run --rm \
+		--volume "$DIR":"$DOCKER_HOME" \
+		"$IMAGE_NAME" npm run "${@:2}"
 else
 	echo "usage: docker_build [init | build | run]"
 fi
