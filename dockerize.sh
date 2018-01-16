38f36f94e41f2bb63d83d4fb13de76239ccb9381
Enoch Tsang <etsang1@hotmail.com>
2018-01-16 15:33:58 -0700

    dockerize script, better control over db container

--- dockerize.sh
+++ dockerize.sh
@@ -11,25 +11,28 @@ elif [ "$1" == "install" ]; then
         --volume "$DIR":"$DOCKER_HOME" \
         "$IMAGE_NAME" yarn install --frozen-lockfile
 elif [ "$1" == "initdb" ]; then
-    docker run --name agricore-postgres \
-        -e POSTGRES_DB=agricore-dev \
+    docker run --name agricore_db \
+        -e POSTGRES_DB=agricore_dev \
         -e POSTGRES_USER=boresha \
         -e POSTGRES_PASSWORD=devenv \
         -e PGDATA="$DIR/data" \
-        --ip 172.17.0.99
-        -d postgres
-    docker kill agricore-postgres
+        -d postgres:10.1
+    docker kill agricore_db
 elif [ "$1" == "start" ]; then
-    docker start agricore-postgres
+    docker start agricore_db
     docker run --rm \
+        --name agricore \
         --volume "$DIR":"$DOCKER_HOME" \
-        --link agricore-postgres:postgres \
+        --link agricore_db:postgres \
         -p 9090:9090 \
         $IMAGE_NAME npm start
+elif [ "$1" == "stop" ]; then
+    docker kill agricore
+    docker kill agricore_db
 elif [ "$1" == "run" ]; then
     docker run --rm \
         --volume "$DIR":"$DOCKER_HOME" \
         "$IMAGE_NAME" npm run "${@:2}"
 else
-    echo "usage: $0 [ init | install | start | run [args] ]"
+    echo "usage: $0 [ init | install | initdb | start | stop | run [args] ]"
 fi
