7fd260db459bb71c7a68e9f7c9a93fea636cf921
Enoch Tsang <etsang1@hotmail.com>
2018-01-16 16:47:59 -0700

    dockerize script initailizes db

--- dockerize.sh
+++ dockerize.sh
@@ -4,35 +4,48 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 IMAGE_NAME="boresha/agricore"
 DOCKER_HOME="/agricore/"
 
+DB_CONTAINER_NAME="agricore_db"
+DB_NAME="agricore_dev"
+DB_USER="boresha"
+DB_PASSWORD="devenv"
+
 if [ "$1" == "init" ]; then
     docker build -t $IMAGE_NAME $DIR
 elif [ "$1" == "install" ]; then
     docker run --rm \
         --volume "$DIR":"$DOCKER_HOME" \
         "$IMAGE_NAME" yarn install --frozen-lockfile
-elif [ "$1" == "initdb" ]; then
-    docker run --name agricore_db \
-        -e POSTGRES_DB=agricore_dev \
-        -e POSTGRES_USER=boresha \
-        -e POSTGRES_PASSWORD=devenv \
-        -e PGDATA="$DIR/data" \
-        -d postgres:10.1
-    docker kill agricore_db
 elif [ "$1" == "start" ]; then
-    docker start agricore_db
     docker run --rm \
         --name agricore \
         --volume "$DIR":"$DOCKER_HOME" \
-        --link agricore_db:postgres \
+        --link "$DB_CONTAINER_NAME":postgres \
         -p 9090:9090 \
         $IMAGE_NAME npm start
 elif [ "$1" == "stop" ]; then
     docker kill agricore
-    docker kill agricore_db
+    docker kill "$DB_CONTAINER_NAME"
 elif [ "$1" == "run" ]; then
     docker run --rm \
         --volume "$DIR":"$DOCKER_HOME" \
         "$IMAGE_NAME" npm run "${@:2}"
+elif [ "$1" == "initdb" ]; then
+    docker run --name "$DB_CONTAINER_NAME" \
+        -e POSTGRES_DB="$DB_NAME" \
+        -e POSTGRES_USER="$DB_USER" \
+        -e POSTGRES_PASSWORD="$DB_PASSWORD" \
+        -e PGDATA="$DIR/data" \
+        -d postgres:10.1
+    docker cp agricore_base.sql agricore_db:/
+    sleep 5
+    docker exec -it "$DB_CONTAINER_NAME" /bin/bash -c "psql -U $DB_USER $DB_NAME < /agricore_base.sql"
+    docker kill "$DB_CONTAINER_NAME"
+elif [ "$1" == "startdb" ]; then
+    docker start "$DB_CONTAINER_NAME"
+elif [ "$1" == "stopdb" ]; then
+    docker kill "$DB_CONTAINER_NAME"
+elif [ "$1" == "shelldb" ]; then
+    docker exec -it "$DB_CONTAINER_NAME" psql -d "$DB_NAME" -U "$DB_USER"
 else
-    echo "usage: $0 [ init | install | initdb | start | stop | run [args] ]"
+    echo "usage: $0 [ init | install | initdb | startdb | shelldb | stopdb | start | stop | run [args] ]"
 fi
