be640d6cd7639626a9fc0483d97801ed831523a0
Enoch Tsang <etsang1@hotmail.com>
2018-01-25 23:17:34 -0700

    Add ProductsTypes API Documentation
    Product API document is created in apidoc format.
    API gives associated attributes for all product types.
    Function stub is created at route /products.
    Route added to route index file.

---
+++ src/routers/products.ts
@@ -0,0 +1,34 @@
+import * as express from 'express';
+
+import createRouter from '@/utilities/functions/createRouter';
+import authorized from '@/middleware/authorized';
+import { UserType } from '@/models/User/UserType';
+
+import { StatusCode } from '@/models/statusCodes';
+
+const router = createRouter();
+
+/**
+ * @api {get} /peopleCategories Get All Product Types
+ * @apiName GetProductTypes
+ * @apiGroup ProductTypes
+ * @apiVersion  0.0.1
+ * @apiDescription Returns all types of products and their associated attributes
+ *
+ * @apiSuccess (200) {String} Success Successfully retrieved all product types
+ * @apiSuccessExample Success-Response:
+  {
+    milk: [
+      "density",
+      "viscosity"
+    ],
+    corn: [
+      "colour",
+    ]
+  }
+ */
+router.get('/', async (req, res) => {
+  res.status(StatusCode.OK).send('Successfully retrieved all product types');
+}, authorized(UserType.ADMIN));
+
+export default router;
