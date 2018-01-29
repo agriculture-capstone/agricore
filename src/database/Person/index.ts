38fdb8cde4e6ebd62ae8dfc9135a18109f9d211a
Enoch Tsang <etsang1@hotmail.com>
2018-01-28 23:12:27 -0700

    ac-55 Respond to PR comments
    Throw errors instead of logging, since they'll either get handled or logged as a crash.
    Adjust switch case to more best practices.

--- src/database/Person/index.ts
+++ src/database/Person/index.ts
@@ -1,5 +1,4 @@
 import dbConnection, { tableNames, execute } from '../connection';
-import logger from '@/utilities/modules/logger';
 import { UserType } from '@/models/User/UserType';
 
 const personAttributesTable = () => dbConnection()(tableNames.PERSON_ATTRIBUTES);
@@ -58,38 +57,38 @@ const builders = {
 /** Build a UserType object from a string */
 function buildUserType(type: string): UserType {
   switch (type) {
-    case UserType.ADMIN:
+    case UserType.ADMIN: {
       return UserType.ADMIN;
-    case UserType.MONITOR:
+    }
+    case UserType.MONITOR: {
       return UserType.MONITOR;
-    case UserType.TRADER:
+    }
+    case UserType.TRADER: {
       return UserType.TRADER;
+    }
+    default: {
+      throw new Error('Unrecognized user type, ' + type);
+    }
   }
-  logger.error('Unrecognized user type', type);
 }
 
 /** Get attributes for a user necessary for a login from a username */
 export async function findUser(username: string): Promise<User> {
-  logger.info('Person/index.ts findUser');
-
   const uuidResults = await execute<PersonUuid[]>(builders.findUserUuid(username));
   if (uuidResults.length === 0) {
     throw new Error('Username ' + username + ' not found');
   }
   const uuid = uuidResults[0].personuuid;
-  logger.info('uuid, ', uuid);
 
   const hashResults = await execute<AttrValue[]>(builders.getAttr(uuid, 'hash'));
   if (hashResults.length === 0) {
     throw new Error(username + ' has no password set!');
   }
-  logger.info('hash, ', hashResults[0].attrvalue);
 
   const userTypeResults = await execute<PersonCategoryName[]>(builders.getPersonCategory(uuid));
   if (userTypeResults.length === 0) {
     throw new Error(username + '  has no category set!');
   }
-  logger.info('userType, ', userTypeResults[0].personcategoryname);
 
   return {
     uuid,
